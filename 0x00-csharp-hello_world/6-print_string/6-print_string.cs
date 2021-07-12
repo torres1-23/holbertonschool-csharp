4d17238c910f5072dcbc59178c7a42b0de0f435b
torres1-23 <alejo.torres1247@gmail.com>
2021-07-12 17:09:12 +0000

    Correct spacing

--- 0x00-csharp-hello_world/6-print_string/6-print_string.cs
+++ 0x00-csharp-hello_world/6-print_string/6-print_string.cs
@@ -7,6 +7,6 @@ class Program
 	{
 		string str = "Holberton School";
 		Console.WriteLine($"{str}{str}{str}");
-        Console.WriteLine($"{str.Substring(0,9)}");
-    }
+		Console.WriteLine($"{str.Substring(0,9)}");
+	}
 }
