178105caa401f55d30ab6149e2d1c13f2de08a50
torres1-23 <alejo.torres1247@gmail.com>
2021-07-12 17:44:23 +0000

    Add specified formatted string

---
+++ 0x00-csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -0,0 +1,13 @@
+﻿  
+using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		double percent = .7553;
+		double currency = 98765.4321;
+		Console.WriteLine($"Percent: {percent:p2}");
+		Console.WriteLine($"Currency: {currency:c2}");
+	}
+}
