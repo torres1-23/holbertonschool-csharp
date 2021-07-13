6b361d528d39922478fbf3d5abb244c54d8b682a
torres1-23 <alejo.torres1247@gmail.com>
2021-07-13 15:31:05 +0000

    Add programm that writes the lowercase abc except letters q and e

---
+++ 0x01-csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		for (int i = 97; i < 97 + 26; i++)
+		{
+			if (i != 101 && i != 113)
+			{
+				Console.Write((char)i);
+			}
+		}
+	}
+}
