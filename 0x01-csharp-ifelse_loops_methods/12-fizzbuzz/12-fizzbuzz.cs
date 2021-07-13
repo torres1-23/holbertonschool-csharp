785aa39de370110f443835e6ec211b3b01ed4dbd
torres1-23 <alejo.torres1247@gmail.com>
2021-07-13 17:43:00 +0000

    Add FizzBuzz algorithm

---
+++ 0x01-csharp-ifelse_loops_methods/12-fizzbuzz/12-fizzbuzz.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		int i = 1;
+		for (; i < 100; i++)
+		{
+			if (i % 3 == 0 && i % 5 == 0)
+			{
+				Console.Write("FizzBuzz ");
+			}
+			else if (i % 3 == 0)
+			{
+				Console.Write("Fizz ");
+			}
+			else if (i % 5 == 0)
+			{
+				Console.Write("Buzz ");
+			}
+			else
+			{
+				Console.Write($"{i} ");
+			}
+		}
+		Console.WriteLine("Buzz");
+	}
+}
