bdb145693e513c463b81d6c25f5f2f3e58722eb4
torres1-23 <alejo.torres1247@gmail.com>
2021-07-13 16:01:51 +0000

    Add program to print smallest combination of double digits number

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		for (int i = 0; i < 8; i++)
+		{
+			for(int j = 0; j <= 9; j++)
+			{
+				if (i != j && i < j)
+				{
+					Console.Write($"{i}{j}, ");
+				}
+			}
+		}
+		Console.WriteLine("89");
+	}
+}
