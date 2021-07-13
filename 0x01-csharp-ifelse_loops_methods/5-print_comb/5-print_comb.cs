6bc99a133f56d738a29fe551475f7038066baaa6
torres1-23 <alejo.torres1247@gmail.com>
2021-07-13 15:51:16 +0000

    Add program that prinst numbers with two digits

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		int i = 0;
+		for (; i < 99; i++)
+		{
+			Console.Write($"{i:d2}, ");
+		}
+		Console.WriteLine($"{i:d2}");
+	}
+}
