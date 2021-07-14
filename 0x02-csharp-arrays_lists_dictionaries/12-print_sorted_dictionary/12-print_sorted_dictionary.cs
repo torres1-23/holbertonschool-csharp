3469f68fc626ce61a9694bdd9c114865c311f8e7
torres1-23 <alejo.torres1247@gmail.com>
2021-07-14 17:41:10 +0000

    Add program that prints keys, values of a dictionary ordered by keys

---
+++ 0x02-csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+	public static void PrintSorted(Dictionary<string, string> myDict)
+	{
+		var KeysList = new List<string>();
+		foreach (string key in myDict.Keys)
+		{
+			KeysList.Add(key);
+		}
+		KeysList.Sort();
+		foreach (string key in KeysList)
+		{
+			Console.WriteLine($"{key}: {myDict[key]}");
+		}
+	}
+}
