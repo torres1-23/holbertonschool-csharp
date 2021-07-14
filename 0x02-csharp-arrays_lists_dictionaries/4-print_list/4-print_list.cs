9aca7adc667430bfb1c074faa99893b1f715bad1
torres1-23 <alejo.torres1247@gmail.com>
2021-07-14 15:51:00 +0000

    Add program that creates a list of integers and prints int

---
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+	public static List<int> CreatePrint(int size)
+	{
+		if (size == 0)
+		{
+			Console.WriteLine("");
+			return new List<int>();
+		}
+		if (size < 0)
+		{
+			Console.WriteLine("Size cannot be negative");
+			return null;
+		}
+		var MyList = new List<int>();
+		for (int i = 0; i < size; i++)
+		{
+			MyList.Add(i);
+		}
+		Console.WriteLine(string.Join(", ", MyList));
+		return MyList;
+	}
+}
