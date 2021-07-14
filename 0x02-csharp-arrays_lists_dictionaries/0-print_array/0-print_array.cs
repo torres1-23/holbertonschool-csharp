6973abb2beb93923f516ca515d830f7700c3a8b2
torres1-23 <alejo.torres1247@gmail.com>
2021-07-14 15:00:47 +0000

    Add program thatprints an array

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Array
+{
+	 public static int[] CreatePrint(int size)
+	{
+		if (size == 0)
+		{
+			Console.WriteLine("");
+			return new int[0];
+		}
+		if (size < 0)
+		{
+			Console.WriteLine("Size cannot be negative");
+			return null;
+		}
+		int[] MyArray = new int[size];
+		for (int i = 0; i < size; i++)
+		{
+			MyArray[i] = i;
+		}
+		Console.WriteLine($"{string.Join(", ", MyArray)}");
+		return MyArray;
+	}
+}
