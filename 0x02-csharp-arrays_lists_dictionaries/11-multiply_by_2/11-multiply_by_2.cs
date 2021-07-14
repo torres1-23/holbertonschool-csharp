9f9c5e835cc0428cf6c00f5ec7b3d56d6dcd3c4d
torres1-23 <alejo.torres1247@gmail.com>
2021-07-14 17:06:23 +0000

    Add program that returns a new dictionary with all values mult by 2

---
+++ 0x02-csharp-arrays_lists_dictionaries/11-multiply_by_2/11-multiply_by_2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+	public static Dictionary<string, int> MultiplyBy2(Dictionary<string, int> myDict)
+	{
+		Dictionary<string, int> MultDict = new Dictionary<string, int>();
+		foreach (KeyValuePair<string, int> pair in myDict)
+		{
+			MultDict[pair.Key] = pair.Value * 2;
+		}
+		return MultDict;
+	}
+}
