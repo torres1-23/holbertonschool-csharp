648f43a22f62c5c7cb8dba253e39a29c4acc2a3e
torres1-23 <alejo.torres1247@gmail.com>
2021-07-14 16:37:41 +0000

    Add program that prints number of keys in a dictionary

---
+++ 0x02-csharp-arrays_lists_dictionaries/8-number_keys/8-number_keys.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+	public static int NumberOfKeys(Dictionary<string, string> myDict)
+	{
+		int i = 0;
+		foreach (string key in myDict.Keys)
+		{
+			i++;
+		}
+		return i;
+	}
+}
