c1b65596dc397ad888d1feeacb191558ca07e555
torres1-23 <alejo.torres1247@gmail.com>
2021-07-14 18:02:30 +0000

    Add program that prints the key of the max value of a dictionary

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+	public static string BestScore(Dictionary<string, int> myList)
+	{
+		var ValueList = new List<int>();
+		foreach (KeyValuePair<string, int> pair in myList)
+		{
+			ValueList.Add(pair.Value);
+		}
+		ValueList.Sort();
+		foreach (KeyValuePair<string, int> pair in myList)
+		{
+			if (pair.Value == ValueList[ValueList.Count - 1])
+			{
+				return pair.Key;
+			}
+		}
+		return "None";
+	}
+}
