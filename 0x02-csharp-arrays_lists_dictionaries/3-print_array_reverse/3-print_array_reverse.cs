2c6fc187390776380a984c1a2014f71347db555a
torres1-23 <alejo.torres1247@gmail.com>
2021-07-14 15:32:25 +0000

    Add program that prints a list in reverse order

---
+++ 0x02-csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Array
+{
+	public static void Reverse(int[] array)
+	{
+		if (array == null || array.Length == 0)
+		{
+			Console.WriteLine("");
+			return;
+		}
+		for (int i = array.Length - 1; i > 0; i--)
+		{
+			Console.Write($"{array[i]}, ");
+		}
+		Console.WriteLine($"{array[0]}");
+	}
+}
