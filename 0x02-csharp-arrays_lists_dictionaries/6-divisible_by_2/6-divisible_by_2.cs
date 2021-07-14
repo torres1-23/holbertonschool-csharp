27b1a8380f81319fe98d53b551296cf5e34613d4
torres1-23 <alejo.torres1247@gmail.com>
2021-07-14 16:17:34 +0000

    Add program that prints if integer on a list are divisible by 2

---
+++ 0x02-csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+	public static List<bool> DivisibleBy2(List<int> myList)
+	{
+		var DivList = new List<bool>();
+		foreach (int number in myList)
+		{
+			if (number % 2 == 0)
+			{
+				DivList.Add(true);
+			}
+			else
+			{
+				DivList.Add(false);
+			}
+		}
+		return DivList;
+	}
+}
