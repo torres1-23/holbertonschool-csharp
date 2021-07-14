5f9444e03b24b74efa32e5f65406feb16155333d
torres1-23 <alejo.torres1247@gmail.com>
2021-07-14 17:14:00 +0000

    Update to return same array if size is 0

--- 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -4,17 +4,17 @@ class Array
 {
 	 public static int[] CreatePrint(int size)
 	{
-		if (size == 0)
-		{
-			Console.WriteLine("");
-			return new int[0];
-		}
 		if (size < 0)
 		{
 			Console.WriteLine("Size cannot be negative");
 			return null;
 		}
 		int[] MyArray = new int[size];
+		if (size == 0)
+		{
+			Console.WriteLine("");
+			return MyArray;
+		}
 		for (int i = 0; i < size; i++)
 		{
 			MyArray[i] = i;
