fdec3efb2cb0180e81f3dcabdb9bc18498a7e078
torres1-23 <alejo.torres1247@gmail.com>
2021-07-15 14:19:55 +0000

    Add program that adds non duplicate numbers in a list

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+	public static int Sum(List<int> myList)
+	{
+		int sum = 0;
+        List<int> NoDupList = myList;
+        NoDupList.Sort();
+        for (int i = 0; i < NoDupList.Count; i++)
+        {
+            if (i == 0 || NoDupList[i] != NoDupList[i - 1])
+			{
+                sum += NoDupList[i];
+			}
+        }
+        return sum;
+	}
+}
