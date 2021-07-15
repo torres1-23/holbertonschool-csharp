d68c8419c740418c31e58505f5774acec659117d
torres1-23 <alejo.torres1247@gmail.com>
2021-07-15 16:59:23 +0000

    Add program that prints the index of a value in a linked list

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+	public static int FindNode(LinkedList<int> myLList, int value)
+	{
+		int i = 0;
+		foreach (int number in myLList)
+		{
+			if (value == number)
+			{
+				return i;
+			}
+			i++;
+		}
+		return -1;
+	}
+}
