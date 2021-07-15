311a8efc323de3e596db7405a6da2a31b59aa43f
torres1-23 <alejo.torres1247@gmail.com>
2021-07-15 02:07:54 +0000

    Add program that prints multiD arrays

---
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		int[,] MultiArray = new int[5,5];
+        MultiArray[2, 2] = 1;
+		for(int i = 0; i < MultiArray.GetLength(0); i++)
+		{
+			for(int j = 0; j <  MultiArray.GetLength(1) - 1; j++)
+			{
+				Console.Write($"{MultiArray[i, j]} ");
+			}
+			Console.WriteLine($"{MultiArray[MultiArray.GetLength(0) - 1, MultiArray.GetLength(1) - 1]}");
+		}
+	}
+}
