8e2dc99e287a63de5acfce9f3c443bfc84027f15
torres1-23 <alejo.torres1247@gmail.com>
2021-07-15 02:23:22 +0000

    Add program that creates and prints a jagged array

---
+++ 0x02-csharp-arrays_lists_dictionaries/100-jagged_array/100-jagged_array.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		int [][] jagger = new int[][]
+        {
+            new int[] {0, 1, 2, 3},
+            new int[] {0, 1, 2, 3, 4, 5, 6},
+            new int[] {0,1}
+        };
+        for (int i = 0; i < jagger.Length; i++)
+        {
+            for (int j = 0; j < jagger[i].Length - 1; j++)
+            {
+                Console.Write($"{jagger[i][j]} ");
+            }
+            Console.WriteLine(jagger[i][jagger[i].Length - 1]);
+        }
+	}
+}
