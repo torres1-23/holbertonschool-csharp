ae994352d25affc39ed00a6b270ffb45eebf99b0
torres1-23 <alejo.torres1247@gmail.com>
2021-07-15 14:41:51 +0000

    Add program that prints double items in two lists

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/1-common_element/1-common_element.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+	public static List<int> CommonElements(List<int> list1, List<int> list2)
+	{
+		List<int> DoubItemsList = new List<int>();
+		list1.Sort();
+		list2.Sort();
+		for (int i = 0; i < list1.Count; i++)
+		{
+			for (int j = 0; j < list2.Count; j++)
+			{
+				if (list1[i] == list2[j])
+				{
+					DoubItemsList.Add(list1[i]);
+				}
+			}
+		}
+		return DoubItemsList;
+	}
+}
