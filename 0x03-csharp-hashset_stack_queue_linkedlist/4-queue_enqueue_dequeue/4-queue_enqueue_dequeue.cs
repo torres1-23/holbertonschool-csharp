d4beeac24fa1b3aa968b5a96a0c7f2d9db5effbf
torres1-23 <alejo.torres1247@gmail.com>
2021-07-15 16:01:29 +0000

    Add program that shows a queue info

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyQueue
+{
+	public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+	{
+		int size = aQueue.Count;
+		bool IsIn = aQueue.Contains(search);
+		Console.WriteLine($"Number of items: {size}");
+		if (size == 0)
+		{
+			Console.WriteLine("Queue is empty");
+		}
+		else
+		{
+			Console.WriteLine($"First item: {aQueue.Peek()}");
+		}
+		aQueue.Enqueue(newItem);
+		Console.WriteLine($"Queue contains {search} {IsIn}");
+		if (IsIn == true)
+		{
+			while (aQueue.Dequeue() != search)
+			{
+				continue;
+			}
+		}
+		return aQueue;
+	}
+}
