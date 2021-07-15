5224bf49b4c4f147e761df67ad704aab0d90a5e9
torres1-23 <alejo.torres1247@gmail.com>
2021-07-15 15:38:41 +0000

    Add program that prints info about a stack

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyStack
+{
+	public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+	{
+		int size = aStack.Count;
+		bool IsIn = aStack.Contains(search);
+		Console.WriteLine($"Number of items: {size}");
+		if (aStack.Count == 0)
+		{
+			Console.WriteLine("Stack is empty");
+		}
+		else
+		{
+			Console.WriteLine($"Top item: {aStack.Peek()}");
+		}
+		Console.WriteLine($"Stack contains {search} {IsIn}");
+		if (IsIn == true)
+		{
+			while (aStack.Pop() != search)
+			{
+				continue;
+			}
+		}
+		aStack.Push(newItem);
+		return aStack;
+	}
+}
