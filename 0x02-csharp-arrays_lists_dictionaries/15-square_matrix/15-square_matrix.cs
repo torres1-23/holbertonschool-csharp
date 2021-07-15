baa4a378525ecad14f1fa31f42b7350e89a5bb89
torres1-23 <alejo.torres1247@gmail.com>
2021-07-15 02:17:45 +0000

    Add program that return a square value matrix

---
+++ 0x02-csharp-arrays_lists_dictionaries/15-square_matrix/15-square_matrix.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Matrix
+{
+	public static int[,] Square(int[,] myMatrix)
+	{
+		int height =myMatrix.GetLength(0);
+		int width = myMatrix.GetLength(1);
+		int[,] MultArray = new int[height, width];
+		for(int i = 0; i < height; i++)
+		{
+			for(int j = 0; j < width; j++)
+			{
+				MultArray[i, j] = myMatrix[i, j] * myMatrix[i, j];
+			}
+		}
+		return MultArray;
+	}
+}
