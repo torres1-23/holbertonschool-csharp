969b195fca47a7742734533eb911b50b5b319e52
torres1-23 <alejo.torres1247@gmail.com>
2021-07-15 17:40:11 +0000

    Add program that prints value of node at a given index

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class LList
+{
+	public static int GetNode(LinkedList<int> myLList, int n)
+	{
+		if (n < 0 || n > myLList.Count)
+		{
+			return 0;
+		}
+		return myLList.ElementAt(n);
+	}
+}
