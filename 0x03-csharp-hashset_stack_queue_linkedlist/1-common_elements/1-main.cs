169d732694f749c5e2c8405565bedf64793ee9d2
torres1-23 <alejo.torres1247@gmail.com>
2021-07-15 17:09:10 +0000

    Update project name

--- 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-main.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-main.cs
@@ -7,7 +7,7 @@ class Program
     static void Main(string[] args)
     {
         List<int> myList1 = new List<int>() {1, 2, 3, 4, 5, 6};
-        List<int> myList2 = new List<int>() {0, 2, 4, 6, 7, 8, 9, 5};
+        List<int> myList2 = new List<int>() {0, 2, 4, 6, 7, 8};
         List<int> newList = new List<int>();
 
         newList = List.CommonElements(myList1, myList2);
