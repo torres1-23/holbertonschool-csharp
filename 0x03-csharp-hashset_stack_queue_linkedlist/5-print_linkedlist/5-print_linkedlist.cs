4b0a1b2a19adb02ac33398cbb10f07d12bde6552
torres1-23 <alejo.torres1247@gmail.com>
2021-07-15 17:22:26 +0000

    Fix edge case

--- 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -6,6 +6,10 @@ class LList
 	public static LinkedList<int> CreatePrint(int size)
 	{
 		LinkedList<int> IntLList = new LinkedList<int>();
+		if (size < 0)
+		{
+			return IntLList;
+		}
 		for (int i = 0; i < size; i++)
 		{
 			IntLList.AddLast(i);
