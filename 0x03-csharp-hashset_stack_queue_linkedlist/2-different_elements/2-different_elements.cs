fec461fdce050e93edafb8e40b40ece2329e2c86
torres1-23 <alejo.torres1247@gmail.com>
2021-07-15 15:16:17 +0000

    Add program that prints no double numbers in two lists

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+	public static List<int> DifferentElements(List<int> list1, List<int> list2)
+	{
+		List<int> NoDupList = new List<int>();
+        foreach (int number in list1)
+        {
+            if (list2.Contains(number))
+			{
+                continue;
+			}
+            else
+			{
+                NoDupList.Add(number);
+			}
+        }
+        foreach (int number in list2)
+        {
+            if (list1.Contains(number))
+			{
+                continue;
+			}
+            else
+			{
+                NoDupList.Add(number);
+			}
+        }
+        NoDupList.Sort();
+        return (NoDupList);
+    }
+}
