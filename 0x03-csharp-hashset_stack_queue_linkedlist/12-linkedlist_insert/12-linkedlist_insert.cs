f01416ba9f5a16772d2b0ced4f626648cf96fcb9
torres1-23 <alejo.torres1247@gmail.com>
2021-07-16 02:04:10 +0000

    Add program that inserts an integer in a sorted linked list

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class LList
+{
+	public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+	{
+		int size = myLList.Count;
+        LinkedListNode<int> head = myLList.First;
+		if (size == 0)
+		{
+			myLList.AddFirst(n);
+			return myLList.First;
+		}
+		while (head.Next != null)
+		{
+			if (head.Value < n)
+			{
+				head = head.Next;
+			}
+			else
+			{
+				myLList.AddBefore(head, n);
+				return myLList.Find(n);
+			}
+		}
+		myLList.AddLast(n);
+		return myLList.First;
+	}
+}
