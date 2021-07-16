5643cfa9fd939a2e9f035d3c460b0081e6483803
torres1-23 <alejo.torres1247@gmail.com>
2021-07-16 02:30:13 +0000

    Add program that deletes a node at a given index

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+	public static void Delete(LinkedList<int> myLList, int index)
+	{
+        LinkedListNode<int> head = myLList.First;
+        for (int i = 0; i < index; i++)
+		{
+            head = head.Next;
+		}
+        myLList.Remove(head);
+	}
+}
