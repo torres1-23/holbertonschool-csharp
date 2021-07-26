4387af60e97ef642108783e26b94604c26e0febd
torres1-23 <alejo.torres1247@gmail.com>
2021-07-26 14:55:22 +0000

    Add program that accepts arguments to create an instance of a struct

---
+++ 0x05-csharp-structs_enums/2-dog/2-dog.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+enum Rating
+{
+	Good,
+	Great,
+	Excellent
+}
+
+struct Dog
+{
+    public Dog (string newName, float newAge, string newOwner, Rating newRating)
+    {
+        name = newName;
+        age = newAge;
+        owner = newOwner;
+        rating = newRating;
+    }
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+}
