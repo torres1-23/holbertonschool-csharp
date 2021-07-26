f64b0e1778c24251992d956f75eab2787f7e6fdd
torres1-23 <alejo.torres1247@gmail.com>
2021-07-26 14:12:13 +0000

    Add program that divides two list element by element

---
+++ 0x04-csharp-exceptions/2-divide_lists/2-divide_lists.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> Divide(List<int> list1, List<int> list2, int listLength)
+    {
+        List<int> divList = new List<int>();
+        int div = 0;
+        for(int i = 0; i < listLength; i++)
+        {
+            try
+            {
+                div = list1[i]/list2[i];
+                divList.Add(div);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Cannot divide by zero");
+                divList.Add(0);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Out of range");
+            }
+            
+        }
+        return divList;
+    }
+}
