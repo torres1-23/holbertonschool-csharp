52575688cae6012e68fdcf7395581ae222acf02c
torres1-23 <alejo.torres1247@gmail.com>
2021-07-26 13:50:25 +0000

    Add program that divides two integers, handling dividebyzero exception

---
+++ 0x04-csharp-exceptions/1-divide_print/1-divide_print.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Int
+{
+    public static void divide(int a, int b)
+    {
+        int div = 0;
+        try
+        {
+            div = a/b; 
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("Cannot divide by zero");
+        }
+        finally
+        {
+            Console.WriteLine($"{a} / {b} = {div}");
+        }
+    }
+}
