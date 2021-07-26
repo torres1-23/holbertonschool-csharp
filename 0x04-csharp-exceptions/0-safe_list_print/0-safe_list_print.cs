e80cc5cd2b27990dd8b8b3165c1d3cc0010c3276
torres1-23 <alejo.torres1247@gmail.com>
2021-07-26 13:42:47 +0000

    Add program that handles a wrong index of a list

--- 0x04-csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
+++ 0x04-csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -3,20 +3,20 @@ using System.Collections.Generic;
 
 class List
 {
-	public static int SafePrint(List<int> myList, int n)
-	{
-		int i = 0;
-		try
-		{
-			for(; i < n; i++)
-			{
-				Console.WriteLine(myList[i]);
-			}
-		}
-		catch (ArgumentOutOfRangeException)
-		{
-			return i;
-		}
-		return i;
-	}
+    public static int SafePrint(List<int> myList, int n)
+    {
+        int i = 0;
+        try
+        {
+            for(; i < n; i++)
+            {
+                Console.WriteLine(myList[i]);
+            }
+        }
+        catch (ArgumentOutOfRangeException)
+        {
+            return i;
+        }
+        return i;
+    }
 }
