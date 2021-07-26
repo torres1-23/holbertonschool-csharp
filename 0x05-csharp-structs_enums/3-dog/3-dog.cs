8722fc61ada5f025c984e157d514c00485fc6e14
torres1-23 <alejo.torres1247@gmail.com>
2021-07-26 15:12:20 +0000

    Add program that overwrites the ToString method of a struct

---
+++ 0x05-csharp-structs_enums/3-dog/3-dog.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+enum Rating
+{
+	Good,
+	Great,
+	Excellent
+}
+
+struct Dog
+{
+    public Dog (string newName, float newAge, string newOwner, Rating newRating)
+    {
+        name = newName;
+        age = newAge;
+        owner = newOwner;
+        rating = newRating;
+    }
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+    public override string ToString()
+    {
+        return ($"Dog Name: {this.name}\n" +
+                $"Age: {this.age}\n" +
+                $"Owner: {this.owner}\n" +
+                $"Rating: {this.rating}");
+    }  
+}
