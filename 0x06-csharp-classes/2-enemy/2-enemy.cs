89678b4f6d639bb3641eb3b347fbe0c88d2adcc6
torres1-23 <alejo.torres1247@gmail.com>
2021-07-27 15:48:29 +0000

    Improve documentation

--- 0x06-csharp-classes/2-enemy/2-enemy.cs
+++ 0x06-csharp-classes/2-enemy/2-enemy.cs
@@ -2,17 +2,17 @@
 
 namespace Enemies
 {
-    ///<summary>Represents zombies enemies.</summary>
+    /// <summary>Represents zombies enemies.</summary>
     public class Zombie
     {
-        ///<summary>Health of a zombie.</summary>
+        /// <summary>Health of a zombie.</summary>
         public int health;
-        ///<summary>Initializes a new instance of the <see cref="Zombie"/> class.</summary>
+        /// <summary>Initializes a new instance of the <see cref="Zombie"/> class.</summary>
         public Zombie()
         {
             health = 0;
         }
-        ///<summary>Initializes a new instance of the <see cref="Zombie"/> class with health value passed as argument.</summary>
+        ///c<summary>Initializes a new instance of the <see cref="Zombie"/> class with health value passed as argument.</summary>
         /// <param name="value">Zombie health value.</param>
         /// <exception cref="ArgumentException"><paramref name="value"/> is less than 0.</exception>
         public Zombie(int value)
