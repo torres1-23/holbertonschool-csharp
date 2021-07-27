a16b36cca9eef4e2128874206ec904f55af89b4c
torres1-23 <alejo.torres1247@gmail.com>
2021-07-27 14:53:51 +0000

    Add program that creates a zombie with 0 health

---
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Enemies
+{
+    ///<summary>Represents zombies enemies.</summary>
+    public class Zombie
+    {
+        ///<summary>Health of a zombie.</summary>
+        public int health;
+        ///<summary>Initializes a new instance of the <see cref="Zombie"/> class.</summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+    }
+}
