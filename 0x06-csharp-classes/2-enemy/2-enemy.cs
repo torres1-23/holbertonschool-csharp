74deaf297e0e74c972d89e2084aae582e887f8a2
torres1-23 <alejo.torres1247@gmail.com>
2021-07-27 15:01:23 +0000

    Add program that can create zombies with health passed as argument

---
+++ 0x06-csharp-classes/2-enemy/2-enemy.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Enemies
+{
+    ///<summary>Represents zombies enemies.</summary>
+    public class Zombie
+    {
+        ///<summary>Health of a zombie.</summary>
+        public int health;
+        ///<summary>Initializes a new instance of the <see cref="Zombie"/> class.</summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+        ///<summary>Initializes a new instance of the <see cref="Zombie"/> class with health value passed as argument.</summary>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            health = value;
+        }
+    }
+}
