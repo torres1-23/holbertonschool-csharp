4cb00e519ca262de166668c3b5e5fb771a2e53b0
torres1-23 <alejo.torres1247@gmail.com>
2021-07-27 15:38:59 +0000

    Change accessibility of health field

--- 0x06-csharp-classes/3-enemy/3-enemy.cs
+++ 0x06-csharp-classes/3-enemy/3-enemy.cs
@@ -5,8 +5,8 @@ namespace Enemies
     /// <summary>Represents zombies enemies.</summary>
     public class Zombie
     {
-        /// <summary>Health of a zombie.</summary>
-        public int health;
+        /// Health of a zombie.
+        private int health;
         /// <summary>Initializes a new instance of the <see cref="Zombie"/> class.</summary>
         public Zombie()
         {
