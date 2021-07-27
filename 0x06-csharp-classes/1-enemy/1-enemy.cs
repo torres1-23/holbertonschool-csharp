299aed4b9beffe932283d844efe9086b812f3e39
torres1-23 <alejo.torres1247@gmail.com>
2021-07-27 15:49:30 +0000

    Improve documentation

--- 0x06-csharp-classes/1-enemy/1-enemy.cs
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -2,12 +2,12 @@
 
 namespace Enemies
 {
-    ///<summary>Represents zombies enemies.</summary>
+    /// <summary>Represents zombies enemies.</summary>
     public class Zombie
     {
-        ///<summary>Health of a zombie.</summary>
+        /// <summary>Health of a zombie.</summary>
         public int health;
-        ///<summary>Initializes a new instance of the <see cref="Zombie"/> class.</summary>
+        /// <summary>Initializes a new instance of the <see cref="Zombie"/> class.</summary>
         public Zombie()
         {
             health = 0;
