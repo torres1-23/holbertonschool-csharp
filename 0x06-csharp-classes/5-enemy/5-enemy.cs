73506ce615e7ed86d7265fc8fcc5e7c223aec4d1
torres1-23 <alejo.torres1247@gmail.com>
2021-07-27 15:47:34 +0000

    Add program that prints a zombie

---
+++ 0x06-csharp-classes/5-enemy/5-enemy.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>Represents zombies enemies.</summary>
+    public class Zombie
+    {
+        // Health of a zombie.
+        private int health;
+        /// <summary>Initializes a new instance of the <see cref="Zombie"/> class.</summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+        /// <summary>Initializes a new instance of the <see cref="Zombie"/> class with health value passed as argument.</summary>
+        /// <param name="value">Zombie health value.</param>
+        /// <exception cref="ArgumentException"><paramref name="value"/> is less than 0.</exception>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            health = value;
+        }
+        /* Name of a zombie.
+        (no name) by default */
+        private string name = "(No name)";
+        /// <summary>Gets or sets the name of a zombie.</summary>
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+        /// <summary>Get the value of the heallth of a zombie.</summary>
+        /// <returns>The value of health of a zombie.</returns>
+        public int GetHealth()
+        {
+            return health;
+        }
+        /// <summary>Overrides ToString Method.</summary>
+        /// <returns>The string representation of a zombie.</returns>
+        public override string ToString()
+        {
+            return $"Zombie Name: {name} / Total Health: {health}";
+        }
+    }
+}
