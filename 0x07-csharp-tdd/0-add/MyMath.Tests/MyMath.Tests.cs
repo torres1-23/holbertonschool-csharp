af142bc7db3d281ad8cb38a6984e17abcff70f06
torres1-23 <alejo.torres1247@gmail.com>
2021-07-29 17:26:58 +0000

    Add project 0

--- 0x07-csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
+++ 0x07-csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
@@ -3,7 +3,7 @@ using NUnit.Framework;
 namespace MyMath.Tests
 {
     [TestFixture]
-    public class Tests
+    public class OperationsTests
     {
         [Test]
         public void Add_Two_Positive()
--- 0x07-csharp-tdd/0-add/MyMath/MyMath.cs
+++ 0x07-csharp-tdd/0-add/MyMath/MyMath.cs
@@ -2,10 +2,10 @@
 
 namespace MyMath
 {
-    ///<summary>Collection of mathematical operations.</summary>
+    /// <summary>Collection of mathematical operations.</summary>
     public class Operations
     {
-        ///<summary>Adds two integers passed as arguments</summary>
+        /// <summary>Adds two integers passed as arguments</summary>
         /// <param name="a">First integer to add.</param>
         /// <param name="b">Second integer to add.</param>
         /// <returns>The addition of two integers</returns>
