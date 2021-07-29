84dc4273c93b68f170771467d679dbcc6b5488e6
torres1-23 <alejo.torres1247@gmail.com>
2021-07-29 17:21:52 +0000

    Add project 0

---
+++ 0x07-csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
@@ -0,0 +1,33 @@
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Add_Two_Positive()
+        {
+            int a = 90;
+            int b = 8;
+            int c = MyMath.Operations.Add(a, b);
+            Assert.AreEqual(a + b, c);
+        }
+        [Test]
+        public void Add_Positive_Negative()
+        {
+            int a = -2;
+            int b = 100;
+            int c = MyMath.Operations.Add(a, b);
+            Assert.AreEqual(a + b, c);
+        }
+        [Test]
+        public void Add_Two_Negative()
+        {
+            int a = -8;
+            int b = -90;
+            int c = MyMath.Operations.Add(a, b);
+            Assert.AreEqual(a + b, c);
+        }
+    }
+}
---
+++ 0x07-csharp-tdd/0-add/MyMath/MyMath.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyMath
+{
+    ///<summary>Collection of mathematical operations.</summary>
+    public class Operations
+    {
+        ///<summary>Adds two integers passed as arguments</summary>
+        /// <param name="a">First integer to add.</param>
+        /// <param name="b">Second integer to add.</param>
+        /// <returns>The addition of two integers</returns>
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
