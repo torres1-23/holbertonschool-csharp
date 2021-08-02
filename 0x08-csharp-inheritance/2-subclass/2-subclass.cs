adf3f8af8aff7398aa18104583e02edd06be0380
torres1-23 <alejo.torres1247@gmail.com>
2021-08-02 14:55:30 +0000

    Add a program that checks if an object is a subclass of an class passed as argument

---
+++ 0x08-csharp-inheritance/2-subclass/2-subclass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary>Represents an Obj.</summary>
+class Obj
+{
+    /// <summary>Checks if an object is an instance or derived of Array class.</summary>
+    /// <param name="derivedType">Object to check.</param>
+    /// <param name="baseType">Class to compare.</param>
+    /// <returns>True if object is an instance of Array, otherwise False.</returns>
+    public static bool IsOnlyASubclass(Type derivedType, Type baseType)
+    {
+        return derivedType.IsSubclassOf(baseType);
+    }
+}
