53d733a6a110193c11c432f326b6a57685217ddb
torres1-23 <alejo.torres1247@gmail.com>
2021-08-02 14:42:47 +0000

    Add a program that checks if an object is of type or inherits from Array class

---
+++ 0x08-csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary>Represents an Obj.</summary>
+class Obj
+{
+    /// <summary>Checks if an object is an instance or derived of Array class.</summary>
+    /// <param name="obj">Object to check.</param>
+    /// <returns>True if object is an instance of Array, otherwise False.</returns>
+    public static bool IsInstanceOfArray(object obj)
+    {
+        return obj is Array;
+    }
+}
