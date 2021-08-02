22025342d98cd99aedfdf40765e5ab2923889d5d
torres1-23 <alejo.torres1247@gmail.com>
2021-08-02 14:36:47 +0000

    Add program that checks te type of an object

---
+++ 0x08-csharp-inheritance/0-is/0-is.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary>Represents an Obj.</summary>
+class Obj
+{
+    /// <summary>Checks if an object is of type int.</summary>
+    /// <param name="obj">Object to check its type.</param>
+    /// <returns>True if object is an int, otherwise False.</returns>
+    public static bool IsOfTypeInt(object obj)
+    {
+        return obj is int;
+    }
+}
