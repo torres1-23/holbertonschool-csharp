b5e1be7d8851c4efedec11614764ea8d88627c8b
torres1-23 <alejo.torres1247@gmail.com>
2021-08-02 16:40:01 +0000

    Add program that prints properties and methods of an object

---
+++ 0x08-csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reflection;
+
+/// <summary>Represents an Obj.</summary>
+class Obj
+{
+    /// <summary>Prints the names of the available properties and methods of an object.</summary>
+    /// <param name="myObj">Object to print its properties.</param>
+    public static void Print(object myObj)
+    {
+        Console.WriteLine($"{myObj.GetType().Name} Properties:");
+        foreach (var property in myObj.GetType().GetProperties())
+        {
+            Console.WriteLine(property.Name);
+        }
+        Console.WriteLine($"{myObj.GetType().Name} Methods:");
+        foreach (var method in myObj.GetType().GetMethods())
+        {
+            Console.WriteLine(method.Name);
+        }
+    }
+}
