05d64cbdb6f9ec03ca9171d5dbbc4bbd514f64e0
torres1-23 <alejo.torres1247@gmail.com>
2021-08-24 01:35:45 +0000

    Add program that computes magnitude of a vector

--- 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -11,11 +11,11 @@ class VectorMath
     {
         if (vector.Length == 2)
         {
-            return Math.Round(Math.Sqrt(Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2)));
+            return Math.Round(Math.Sqrt(Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2)), 2);
         }
         else if (vector.Length == 3)
         {
-            return Math.Round(Math.Sqrt(Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2) + Math.Pow(vector[2], 2)));
+            return Math.Round(Math.Sqrt(Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2) + Math.Pow(vector[2], 2)), 2);
         }
         return -1;
     }
