48209cf7041abe72b9034a5745f875e36705361e
torres1-23 <alejo.torres1247@gmail.com>
2021-08-28 02:33:29 +0000

    Updated documentation

--- 0x09-csharp-linear_algebra/12-dot_product/12-dot_product.cs
+++ 0x09-csharp-linear_algebra/12-dot_product/12-dot_product.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 /// <summary>Represents Mathematical Operations with Vectors.</summary>
 class VectorMath
 {
-    /// <summary>Computesproduct of vector and scalar.</summary>
+    /// <summary>Computes product of vector and scalar.</summary>
     /// <param name="vector1">Vector to multiply.</param>
     /// <param name="vector2">Vector to multiply</param>
     /// <returns>Resulting scalar if operation is possible, -1 otherwise.</returns>
